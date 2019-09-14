8aaf5d93c71529da785c3f69ddcddb21d0be5991
Juan <j.juancamilojurado@gmail.com>
2019-09-13 23:22:29 -0500

    Initial commit

---
+++ Taller4.1/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Taller4._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtnumeros = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnregistrar = new System.Windows.Forms.Button();
+            this.Valor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Numeros = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Total = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtnumeros
+            // 
+            this.txtnumeros.Location = new System.Drawing.Point(12, 89);
+            this.txtnumeros.Name = "txtnumeros";
+            this.txtnumeros.Size = new System.Drawing.Size(100, 20);
+            this.txtnumeros.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Valor,
+            this.Numeros,
+            this.Total});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 183);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(534, 254);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btnregistrar
+            // 
+            this.btnregistrar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnregistrar.Location = new System.Drawing.Point(141, 89);
+            this.btnregistrar.Name = "btnregistrar";
+            this.btnregistrar.Size = new System.Drawing.Size(75, 23);
+            this.btnregistrar.TabIndex = 3;
+            this.btnregistrar.Text = "Registrar";
+            this.btnregistrar.UseVisualStyleBackColor = true;
+            this.btnregistrar.Click += new System.EventHandler(this.Btnregistrar_Click);
+            // 
+            // Valor
+            // 
+            this.Valor.HeaderText = "Valor";
+            this.Valor.Name = "Valor";
+            this.Valor.ReadOnly = true;
+            // 
+            // Numeros
+            // 
+            this.Numeros.HeaderText = "Números";
+            this.Numeros.Name = "Numeros";
+            this.Numeros.ReadOnly = true;
+            // 
+            // Total
+            // 
+            this.Total.HeaderText = "Total";
+            this.Total.Name = "Total";
+            this.Total.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 449);
+            this.Controls.Add(this.btnregistrar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtnumeros);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtnumeros;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnregistrar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Valor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Numeros;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Total;
+    }
+}
+
---
+++ Taller4.1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taller4._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int num=1;
+
+        private void Btnregistrar_Click(object sender, EventArgs e)
+        {
+            int tabla = int.Parse(txtnumeros.Text);
+           // string total;
+            
+
+            dataGridView1.Rows.Add(tabla, num+"");
+
+            num++;
+
+
+            for (int i = tabla; i <= 10; i++)
+            {
+
+                for (int j = tabla; j <= 10; j++)
+                {
+
+                }
+
+            }
+
+           
+
+        }
+    }
+}
